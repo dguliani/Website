a76a9f8b596a5e5eae9e0054bb86cc5e19e0a299
Dhruv Guliani <dhruv.guliani@gmail.com>
2013-06-18 21:00:43 -0400

    setting up index and js

---
+++ js/mainPage.js
@@ -0,0 +1,54 @@
+var mainPage;
+if( !mainPage ) {
+    mainPage = {};
+}
+
+mainPage.application = (function()
+{
+    var application = {},
+
+    //elements
+    mainContainer,
+    headerBar,
+
+    //functions
+    resizeView;
+      
+    onClick = function( e ) 
+    {
+        var target = e.target
+    }
+      
+    resizeView = function( e ) {
+        var availableHeight = window.innerHeight;
+
+        container.style.height = availableHeight - headerBar.offsetHeight + 'px';
+    }
+    
+
+    application.initialize = function()
+    {
+        mainContainer = $('main_container');
+        headerBar = $('header');
+
+        console.log("initializing");
+
+        window.onclick = onClick;
+        window.onresize = resizeView;
+        resizeView();
+
+
+    };
+
+    return application;
+}());
+
+var $ = function( elementID )
+{
+    return document.getElementById( elementID );
+};
+
+// This line starts the application
+// It should be the last line in the last javascript file for this project
+
+window.addEventListener( "DOMContentLoaded", mainPage.application.initialize, true )
